c1648871925d1e7540aac0bdf7bfead07fc77e2d
Mamun-swe <mhmamun166009@gmail.com>
2020-12-30 15:37:07 +0600

    okk

--- src/pages/Account/Patient/Council/Chat/Index.js
+++ src/pages/Account/Patient/Council/Chat/Index.js
@@ -1,13 +1,10 @@
 import React from 'react'
 import './style.scss'
 
-const Index = ({user}) => {
-
-
+const Index = ({ user }) => {
     return (
         <div className="chat-room border">
             <h1>{user.name}</h1>
-
         </div>
     );
 }
